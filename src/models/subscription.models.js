7cc43f87b8f889023866f74e2924310242c7878a
DevanshTomar <tomardevansh83@gmail.com>
2025-08-05 11:41:23 -0700

    Add subscription model with schema and indexes for managing user subscriptions to channels

---
+++ src/models/subscription.models.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose'
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// Ensure a user can't subscribe to the same channel twice
+subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true })
+
+// Index for efficiently finding subscribers of a channel
+subscriptionSchema.index({ channel: 1 })
+
+// Index for efficiently finding channels a user subscribes to
+subscriptionSchema.index({ subscriber: 1 })
+
+export const Subscription = mongoose.model('Subscription', subscriptionSchema)
