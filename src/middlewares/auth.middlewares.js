519c6bbf0e756e043a63bec43d65d362b1aebbca
DevanshTomar <tomardevansh83@gmail.com>
2025-08-12 10:13:18 -0700

    Refactor access token verification middleware; improve error handling and code readability by restructuring token extraction and user validation logic

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,28 +1,35 @@
 import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
+import asyncHandler from '../utils/asyncHandler.js'
 import ApiError from '../utils/ApiError.js'
 import { User } from '../models/user.models.js'
 
 export const verifyAccessToken = asyncHandler(async (req, _, next) => {
-    const accessToken = req.cookies?.accessToken || req.headers?.authorization?.split(' ')[1] || req.body?.accessToken
+  const accessToken =
+    req.cookies?.accessToken ||
+    req.headers?.authorization?.split(' ')[1] ||
+    req.body?.accessToken
 
-    if (!accessToken) {
-        throw new ApiError(401, 'Access token is required')
+  if (!accessToken) {
+    throw new ApiError(401, 'Access token is required')
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET
+    )
+
+    const user = await User.findById(decodedToken?._id).select(
+      '-password -refreshToken'
+    )
+
+    if (!user) {
+      throw new ApiError(401, 'User not found')
     }
 
-    try {
-        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-
-        const user = await User.findById(decodedToken?._id).select('-password -refreshToken')
-
-        if (!user) {
-            throw new ApiError(401, 'User not found')
-        }
-
-        req.user = user
-        next()  
-
-    } catch (error) {
-        throw new ApiError(401, 'Invalid access token: ' + error?.message)
-    }
+    req.user = user
+    next()
+  } catch (error) {
+    throw new ApiError(401, 'Invalid access token: ' + error?.message)
+  }
 })
