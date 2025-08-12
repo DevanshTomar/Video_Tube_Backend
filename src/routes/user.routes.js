c3d88b076c3a68b96b754592a979a4a41126f223
DevanshTomar <tomardevansh83@gmail.com>
2025-08-12 11:33:08 -0700

    Add tweet and video controllers; implement basic CRUD operations with async error handling; create routes for tweet and video management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { registerUser, loginUser, refreshAccessToken, logoutUser } from '../controllers/user.controllers.js'
+import { registerUser, loginUser, refreshAccessToken, logoutUser, changeCurrentPassword, getCurrentUser, userChannelProfile, getWatchHistory, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from '../controllers/user.controllers.js'
 import { verifyAccessToken } from '../middlewares/auth.middlewares.js'
 import { upload } from '../middlewares/multer.middlewares.js'
 
 const router = Router()
+
+//public routes
 
 router.route('/register').post(
   upload.fields([
@@ -17,6 +19,25 @@ router.route('/login').post(loginUser)
 
 router.route('/refresh-access-token').post(refreshAccessToken)
 
+
+//authenticated routes
+
+router.route('/change-password').post(verifyAccessToken, changeCurrentPassword)
+
+router.route('/current-user').get(verifyAccessToken, getCurrentUser)
+
+router.route('/channel-profile/:username').get(verifyAccessToken, userChannelProfile)
+
+router.route('/watch-history').get(verifyAccessToken, getWatchHistory)
+
+router.route('/update-account-details').put(verifyAccessToken, updateAccountDetails)
+
+router.route('/update-avatar').patch(verifyAccessToken, upload.single('avatar'), updateUserAvatar)
+
+router.route('/update-cover-image').patch(verifyAccessToken, upload.single('coverImage'), updateUserCoverImage)
+
 router.route('/logout').post(verifyAccessToken, logoutUser)
+
+
 
 export default router
